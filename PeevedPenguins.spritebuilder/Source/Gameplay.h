adb0bc9be13673ca3dc1d8a79aaa8f5d3a903aac
Totor Ledeb <totorledeb@Totors-Mac-mini.local>
2015-08-05 18:42:45 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Totor Ledeb on 05/08/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
