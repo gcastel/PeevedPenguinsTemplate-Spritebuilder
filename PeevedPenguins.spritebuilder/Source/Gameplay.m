5838c6fbef8f300f491c1209df5cd0f99de24eed
Totor Ledeb <totorledeb@Totors-Mac-mini.local>
2015-10-01 16:00:12 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 
     
@@ -23,6 +24,8 @@
         
         // visualize physics bodies & joints
         //_physicsNode.debugDraw = TRUE;
+        // nothing shall collide with our invisible nodes
+        _pullbackNode.physicsBody.collisionMask = @[];
         
         CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
         [_levelNode addChild:level];
