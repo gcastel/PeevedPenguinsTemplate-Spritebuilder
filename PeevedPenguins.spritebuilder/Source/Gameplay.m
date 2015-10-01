567dd0217781a3e55894cd17bded113a2455e902
Totor Ledeb <totorledeb@Totors-Mac-mini.local>
2015-10-01 15:35:39 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -22,7 +22,7 @@
         self.userInteractionEnabled = TRUE;
         
         // visualize physics bodies & joints
-        _physicsNode.debugDraw = TRUE;
+        //_physicsNode.debugDraw = TRUE;
         
         CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
         [_levelNode addChild:level];
