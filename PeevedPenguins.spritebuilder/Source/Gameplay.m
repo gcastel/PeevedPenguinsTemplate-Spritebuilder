d19b3ec8ed1ba59a035fa0ad923859ba035fa292
Totor Ledeb <totorledeb@Totors-Mac-mini.local>
2015-10-02 14:44:44 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -14,6 +14,8 @@
     CCNode *_levelNode;
     CCNode *_contentNode;
     CCNode *_pullbackNode;
+    CCNode *_mouseJointNode;
+    CCPhysicsJoint *_mouseJoint;
 }
 
     
@@ -26,14 +28,25 @@
         //_physicsNode.debugDraw = TRUE;
         // nothing shall collide with our invisible nodes
         _pullbackNode.physicsBody.collisionMask = @[];
+        _mouseJointNode.physicsBody.collisionMask = @[];
         
         CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
         [_levelNode addChild:level];
     }
     
     // called on every touch in this scene
-    - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-        [self launchPenguin];
+    -(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+        CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+        // start catapult dragging when a touch inside of the catapult arm occurs
+        if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+        {
+            // move the mouseJointNode to the touch position
+            _mouseJointNode.position = touchLocation;
+        
+            // setup a spring joint between the mouseJointNode and the catapultArm
+            _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        }
     }
     
     - (void)launchPenguin {
